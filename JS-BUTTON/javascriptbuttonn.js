72d198c2440a876c3feadd8438da198075e81d0b
Ubuntu 43 <OPTIMUSDOM\ubuntu43@optimus43-hp-dx2480-mt-kl969av.optimusdom.com>
2015-07-06 18:15:41 +0530

    "assignments"

---
+++ JS-BUTTON/javascriptbuttonn.js
@@ -0,0 +1,10 @@
+﻿function change() {
+    var x = document.querySelectorAll("div");
+    var i;
+    for(i = 0;i < x.length;i++) {
+        if(x[i].innerHTML=="find") {
+	    x[i].innerHTML = "found";
+	    x[i].style.background = "lightblue";
+        }
+    }
+}
